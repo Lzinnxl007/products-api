c7e2b4b0b42d976a31b468d3ffea445ca1c2e549
Lzinnxl007 <leosantana.silva27@gmail.com>
2024-07-22 13:03:58 -0300

    finish

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
-import express from "express"
+import express from "express";
+import fs from "node:fs";
+import path from "node:path";
+import { join } from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Define your API routes
-app.get('/api/hello', (req, res) => {
-  res.status(200).json({ message: "Hello, World!" });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(__filename, "..");
+
+const filePath = path.join(__dirname, "products.txt");
+
+app.get("/api/products", (req, res) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading the file:", err);
+      return;
+    }
+
+    try {
+      const products = JSON.parse(data);
+      res.status(200).json(products);
+    } catch (parseError) {
+      console.error("Error parsing JSON:", parseError);
+    }
+  });
 });
 
-// Catch all other routes and return a 404 response
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
