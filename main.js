5ad5b7534ae7f78df9cd9a6f822416ba052dc20b
Lzinnxl007 <leosantana.silva27@gmail.com>
2024-07-22 12:28:32 -0300

    api created

---
+++ main.js
@@ -0,0 +1,15 @@
+import fastify from "fastify"
+import fastifyCors from "@fastify/cors"
+
+const server = fastify()
+server.register(fastifyCors)
+
+server.get("/", (req, reply) => {
+    return "Hello World!"
+})
+
+server.listen({
+    port: 3000,
+}, () => {
+    console.log("Server running...")
+})
